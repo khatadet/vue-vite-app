d57633b8d79824d6c6c27ad5279b14214f6d042c
khatadet <ponpang39170@gmail.com>
2021-08-28 11:11:54 +0700

    init project

---
+++ src/components/pon.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data: () => {
+    return {
+      mas: "20",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Khatadet</h1>
+    
+      <img
+      class= center
+        alt="pon"
+        src="../assets/168733165_2936104476620065_6789039554079085077_n.jpg"
+        width="500"
+        height="600"
+      />
+    
+    <h1>{{ mas }}</h1>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+</style>
